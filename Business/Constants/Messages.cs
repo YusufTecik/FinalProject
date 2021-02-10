b9ed937ef848d83877ae4a57fd4e64b59cbe8cea
yusuf tecik <tecikyusuf732@gmail.com>
2021-02-10 23:15:53 +0200

    10 Şubat Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün ekendi";
+        public static string ProductNameInValid = "Ürün ismi geçersiz";
+        public static string MeintenanceTime = "Sistem Bakımda";
+        public static string ProductListed = "Ürünler Listelendi";
+    }
+}
